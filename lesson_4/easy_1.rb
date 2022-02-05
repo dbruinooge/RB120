bedb9b3564062309ccb010c2b924826dbecd5779
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-02-05 14:02:53 +0000

    Add lesson 4 exercises

---
+++ lesson_4/easy_1.rb
@@ -0,0 +1,8 @@
+# Which of the following are objects in Ruby? If they are objects, how can you find out what class they belong to?
+
+# true
+# "hello"
+# [1, 2, 3, "happy days"]
+# 142
+
+# Answer: All of these are objects in Ruby. You can find out what class they belong to by invoking the #class method on them.
---
+++ lesson_4/easy_3.rb
@@ -0,0 +1,3 @@
+# The class method is called on the object itself (self), and returns
+# the class that the object is an instance of - either Car or Truck.
+# A string representation of that class is then output via string interpolation.
