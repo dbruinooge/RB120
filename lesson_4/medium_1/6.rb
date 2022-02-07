606a10cd8014ceef825b4b8a95ea72a6a23ba0e0
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-02-07 00:01:29 +0000

    Add Medium 1 exercises

---
+++ lesson_4/medium_1/6.rb
@@ -0,0 +1,8 @@
+=begin
+
+In the first case, the template getter and setter methods are invoked without the self. prefix.
+In the second case, the self. prefix is used.
+
+The same methods are invoked in each case and the results are also the same.
+
+=end
