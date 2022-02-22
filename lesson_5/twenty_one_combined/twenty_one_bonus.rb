7ef888a7248aae9d1cf40e126c2636a0181a0a66
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-02-22 02:01:29 +0000

    Update twenty-one bonus features to reflect code review; use generic names for AI

--- lesson_5/twenty_one_combined/twenty_one_bonus.rb
+++ lesson_5/twenty_one_combined/twenty_one_bonus.rb
@@ -90,6 +90,45 @@ module Betable
   end
 end
 
+module Handable
+  CARD_VALUES = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
+                  '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10,
+                  'Q' => 10, 'K' => 10, 'A' => 11 }
+
+  MAX_SCORE = 21
+  ACE_HIGH_SCORE = 11
+  ACE_LOW_SCORE = 1
+
+  attr_reader :hand
+
+  def hit(card)
+    hand << card
+  end
+
+  def list_cards
+    hand.join(', ')
+  end
+
+  def total
+    total = 0
+    hand.each { |card| total += CARD_VALUES[card.value] }
+    aces = hand.select { |card| card.value == 'A' }.count
+    while total > MAX_SCORE && aces > 0
+      total -= (ACE_HIGH_SCORE - ACE_LOW_SCORE)
+      aces -= 1
+    end
+    total
+  end
+
+  def busted?
+    total > MAX_SCORE
+  end
+
+  def discard_hand
+    @hand = []
+  end
+end
+
 class Game
   include Displayable
 
@@ -330,44 +369,11 @@ class Card
 end
 
 class Participant
-  CARD_VALUES = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
-                  '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10,
-                  'Q' => 10, 'K' => 10, 'A' => 11 }
+  include Handable
 
-  MAX_SCORE = 21
-  ACE_HIGH_SCORE = 11
-  ACE_LOW_SCORE = 1
-
-  attr_reader :hand, :name
+  attr_reader :name
 
   def initialize
-    @hand = []
-  end
-
-  def hit(card)
-    hand << card
-  end
-
-  def list_cards
-    hand.join(', ')
-  end
-
-  def total
-    total = 0
-    hand.each { |card| total += CARD_VALUES[card.value] }
-    aces = hand.select { |card| card.value == 'A' }.count
-    while total > MAX_SCORE && aces > 0
-      total -= (ACE_HIGH_SCORE - ACE_LOW_SCORE)
-      aces -= 1
-    end
-    total
-  end
-
-  def busted?
-    total > MAX_SCORE
-  end
-
-  def discard_hand
     @hand = []
   end
 end
@@ -386,10 +392,6 @@ class Dealer < Participant
 
   def hit?
     total < DEALER_STANDS
-  end
-
-  def list_cards
-    hand.join(', ')
   end
 end
 
